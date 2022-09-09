c2d746c73db849c8168800692674a37b5e93816d
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-09-09 17:46:54 +0300

    comm

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ body {
     font-family: 'Open Sans' , 'Helvetica Neue',Helvetica, sans-serif;
     line-height: 1.45;
 }
+header {
+    color: #FFFFFF;
+    background-color: antiquewhite;
+    padding: 80px 0;
+    text-align: center;
+}
+section {
+    padding: 40px 0;
+    text-align: center;
+}
+.assets {
+    background: #868686;
+    padding: 20px;
+    min-height: 20vh;
+}
