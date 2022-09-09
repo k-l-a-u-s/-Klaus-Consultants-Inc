91e267cccf4e6ec036d736b878d30b637725a9b9
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-09-09 17:49:25 +0300

    comm

--- index.js
+++ index.js
@@ -129,4 +129,30 @@ footer {
     cursor: pointer;
     width: 100px;
     height: 40px;
-}
+}const error= document.querySelector('.error');
+const search = document.querySelector('#search');
+search.addEventListener('click', (e) => {
+   const searchbar = document.querySelector("#searchbar").value
+   fetchData(searchbar);
+})
+
+function fetchData(query) {
+   fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?/${query}`)
+    .then((resp) => resp.json())
+    .then((Data) => {
+         if (Data.error) {
+            console.log(Data.error);
+            error.setAttribute('style', 'display: block');
+         }else
+         {
+            error.setAttribute('style', 'display: none');
+    const children= document.querySelector('.data').children;
+    children[0].innerHTML = Data.data.Drink;
+         }
+     
+  })}
+  
+  document.querySelector("._submit_btn").addEventListener("click", (e) => {
+   e.preventDefault();
+   console.log("Submitting");
+ });
