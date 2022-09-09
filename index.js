138318d397fe82aa3d9af5e6e0006cb396dd1948
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-09-09 17:47:44 +0300

    comm

--- index.js
+++ index.js
@@ -29,3 +29,63 @@ h1 {
     font-size: 48px;
     margin: 0 0 16px 0;
 }
+.container {
+    margin: 0 auto;
+    padding: 0 20px;
+    max-width: 900px;
+    background: linear-gradient(to right, rgba(153,158,138,1) 0%, rgba(142,145,126,1) 11%, rgba(137,141,119,1) 33%, rgba(149,152,133,1) 39%, rgba(170,173,159,0.93) 50%, rgba(140,144,122,0.8) 71%, rgba(140,144,122,0.62) 100%);
+}
+
+.section { 
+    text-align: center;
+    padding: 40px 0;
+}
+.features {
+    background: #FFFFFF;
+    color: #000000;
+}
+
+footer {
+    background:#353535;
+    padding: 32px 0;
+    text-align: center;
+    color: #000000;
+}
+.feature {
+    width: 32%;
+    display: inline-block;
+    font-size: 16px;
+}
+
+blockquote {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
+blockquote p {
+    margin: 0 0 5px 0;
+    font-size: 24px;
+}
+
+footer {
+    background: antiquewhite;
+    padding: 32px 0;
+    text-align: center;
+    color:#868686;
+    font-size: 12px;
+}
+.container{
+    margin: 0 auto;
+    padding: 0 20px;
+    max-width: 900px;
+}
+
+.header {
+   display: flex;
+    justify-content: space-between;
+}
+
+.header p{
+    width: 100px;
+
+}
